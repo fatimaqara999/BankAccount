8ea11adcf42391d6c29d2dc07d12459c2f303172
fatimaqara999 <fatimaqara3@gmail.com>
2026-06-19 02:46:19 +0400

    Tapsiriq 2 - Deyisenin deyerini deyisdirin

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class BankAccount {
+    public static void main(String[] args) {
+        String musteriAdi = "Aysel Məmmədova";
+        int hesabNomresi = 1024587;
+        double balans = 3842.50;
+        boolean hesabAktivMi = true;
+        String hesabNovu = "Əmanət";
+
+        System.out.println("Müştəri:       " + musteriAdi);
+        System.out.println("Hesab nömrəsi: " + hesabNomresi);
+        System.out.println("Balans:        " + balans);
+        System.out.println("Hesab aktiv:   " + hesabAktivMi);
+        System.out.println("Hesab növü:    " + hesabNovu);
+    }
 }
