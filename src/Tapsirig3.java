58e0dea98412b0f644d42b2493c795f470fd656c
fatimaqara999 <fatimaqara3@gmail.com>
2026-06-19 10:11:41 +0400

    Tapsiriq 2 - Deyisenin deyerini deyisdirin

---
+++ src/Tapsirig3.java
@@ -0,0 +1,19 @@
+public class Tapsirig3 {
+    public static void main(String[] args) {
+        // final ilə sabitlər — BÖYÜK_HƏRF konvensiyası
+        final int MIN_KREDIT = 300;
+        final int MAX_KREDIT = 50000;
+        final double ILLIK_FAIZ = 10.9;
+        final String BANK_ADI = "ABB";
+        final int MAX_MUDDET = 60;
+
+        // Ekrana çap
+        System.out.println("Min kredit məbləği:   " + MIN_KREDIT + " AZN");
+        System.out.println("Max kredit məbləği:   " + MAX_KREDIT + " AZN");
+        System.out.println("İllik standart faiz:  " + ILLIK_FAIZ);
+        System.out.println("Bankın qısa adı:      " + BANK_ADI);
+        System.out.println("Max kredit müddəti:   " + MAX_MUDDET + " ay");
+
+         // MIN_KREDIT = 500;   //
+    }
+}
