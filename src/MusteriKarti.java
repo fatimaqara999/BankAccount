831dc662934e69af559fbeb84f0758e4b0671d95
fatimaqara999 <fatimaqara3@gmail.com>
2026-06-19 10:29:19 +0400

    MusteriKarti.java

---
+++ src/MusteriKarti.java
@@ -0,0 +1,17 @@
+public class MusteriKarti {
+    public static void main(String[] args) {
+        char bashErf = 'L';                  // char — tək dırnaq ' '
+        String tamAd = "Leyla Hüseynova";    // String — cüt dırnaq " "
+        int yas = 29;
+        double aylikGelir = 2450.75;
+        boolean vipMusteri = true;
+        int musteriKodu = 48213;             // 5 rəqəmli
+
+        System.out.println("Baş hərf:     " + bashErf);
+        System.out.println("Tam ad:       " + tamAd);
+        System.out.println("Yaş:          " + yas);
+        System.out.println("Aylıq gəlir:  " + aylikGelir);
+        System.out.println("VIP müştəri:  " + vipMusteri);
+        System.out.println("Müştəri kodu: " + musteriKodu);
+    }
+}
